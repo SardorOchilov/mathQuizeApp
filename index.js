f1407011b9bb8a6ba5a808787f29b37343cd6529
ochildevo <ochilovsardor45@gmail.com>
2023-03-31 06:00:12 +0500

    fix: repetion questions

--- index.js
+++ index.js
@@ -6,27 +6,49 @@ const optionElements = document.querySelector(".answers").children;
 let currentScore = 0;
 let bestScore = 0;
 const logicalOperators = ["+", "-", "*"];
-const randomOperator = Math.floor(Math.random() * 3);
+let randomOperator = null;
 let nums = [];
 let answerOptionNums = [];
 let answer = null;
 
-function GameContinue() {
-  // Nums Generator Functions
-  function numsMultiplyGenerator() {
-    nums[0] = Math.floor(Math.random() * 20) + 10;
-    nums[1] = Math.floor(Math.random() * 9) + 1;
-    nums.sort(() => Math.random() - 0.5);
-    return nums;
-  }
-  function numsGenerator() {
-    nums[0] = Math.floor(Math.random() * 100) + 1;
-    nums[1] = Math.floor(Math.random() * 100) + 1;
-    return nums;
-  }
+// Nums Generator Functions
+function numsMultiplyGenerator() {
+  nums[0] = Math.floor(Math.random() * 20) + 10;
+  nums[1] = Math.floor(Math.random() * 9) + 1;
+  nums.sort(() => Math.random() - 0.5);
+  return nums;
+}
 
-  // Answers Generator Functions
+function numsGenerator() {
+  nums[0] = Math.floor(Math.random() * 100) + 1;
+  nums[1] = Math.floor(Math.random() * 100) + 1;
+  return nums;
+}
 
+// Answers Generator Functions
+function answersGenerator() {
+  answer = eval(question.textContent.slice(0, -3));
+
+  answerOptionNums[0] = answer;
+  let j = 1;
+  while (j < 4) {
+    answerOptionNums[j] =
+      Math.floor(Math.random() * 100) +
+      Math.floor(Math.random() * 100) -
+      Math.floor(Math.random() * 100);
+    if (answerOptionNums[j] !== answer) {
+      j++;
+    }
+  }
+  answerOptionNums.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < 4; i++) {
+    optionElements[i].textContent = answerOptionNums[i];
+  }
+}
+
+// Question Genrator
+function questionGenerator() {
+  randomOperator = Math.floor(Math.random() * 3);
   if (logicalOperators[randomOperator] === "*") {
     numsMultiplyGenerator();
     question.textContent = `${nums[0]} * ${nums[1]} = ?`;
@@ -34,35 +56,27 @@ function GameContinue() {
     numsGenerator();
     question.textContent = `${nums[0]} ${logicalOperators[randomOperator]} ${nums[1]} = ?`;
   }
-  answersGenerator();
 }
 
-GameContinue();
+questionGenerator();
+answersGenerator();
 
-function answersGenerator() {
-  answer = eval(question.textContent.slice(0, -3));
-
-  answerOptionNums[0] = answer;
-  for (let i = 1; i < 4; i++) {
-    answerOptionNums[i] = Math.floor(Math.random() * answer);
-  }
-  answerOptionNums.sort(() => Math.random() - 0.5);
-
-  for (let i = 0; i < 4; i++) {
-    optionElements[i].textContent = answerOptionNums[i];
-    optionElements[i].addEventListener("click", (e) => {
-      if (+e.target.textContent === answer) {
-        currentScore++;
-        currentScoreElement.textContent = currentScore;
-        GameContinue();
-      } else {
-        if (currentScore > +bestScoreElement.textContent) {
-          bestScoreElement.textContent = currentScore;
-        }
-        currentScore = 0;
-        currentScoreElement.textContent = currentScore;
-        GameContinue();
+// Check answer
+for (let i = 0; i < 4; i++) {
+  optionElements[i].addEventListener("click", (e) => {
+    if (+e.target.textContent === answer) {
+      currentScore++;
+      currentScoreElement.textContent = currentScore;
+      questionGenerator();
+      answersGenerator();
+    } else {
+      if (currentScore > +bestScoreElement.textContent) {
+        bestScoreElement.textContent = currentScore;
       }
-    });
-  }
+      currentScore = 0;
+      currentScoreElement.textContent = currentScore;
+      questionGenerator();
+      answersGenerator();
+    }
+  });
 }
